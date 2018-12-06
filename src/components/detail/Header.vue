96fb05f83a2d7c79b55ec6610f507770425c5045
xiaohonga <xioaming@qq.com>
2018-12-06 15:13:14 +0800

    fix problem

--- src/components/detail/Header.vue
+++ src/components/detail/Header.vue
@@ -41,6 +41,9 @@
         },
         activated() {
             window.addEventListener('scroll', this.handleScroll)
+        },
+        deactivated(){
+            window.removeEventListener('scroll',this.handleScroll)
         }
     }
 </script>
